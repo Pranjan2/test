c111760be3c8b5d2b195fa00910eca77c717f0b8
Prateek <pranjan2@illinois.edu>
2018-10-02 00:05:36 -0500

    GitHub test file

---
+++ Hello_world/Hello_world/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Hello_world
+//
+//  Created by Prateek Ranjan on 10/2/18.
+//  Copyright © 2018 Prateek Ranjan. All rights reserved.
+//
+
+#include <iostream>
+#include <Eigen/Dense>
+
+int main(int argc, const char * argv[]) {
+
+    std::cout << " First program on GutHub \n ";
+    
+    std::cout << " Hello World !";
+    
+    std::cin.get();
+    
+    return 0;
+}
